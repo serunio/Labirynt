c981b2e047c5f8c196e374aec3398ad664844fda
serunio <jak.haraszkiewicz@gmail.com>
2023-12-31 17:37:03 +0100

    rzeczy

--- DFS.h
+++
@@ -1,6 +0,0 @@
-
-#ifndef LABIRYNT_DFS_H
-#define LABIRYNT_DFS_H
-int generacja_dfs(komorka_t** k, int x, int y, int seed); //funkcja rekurencyjna. tworzy przejscie do losowej komorki i wywoluje sie w niej. zwraca 1 w razie sukcesu i 0 w razie porazki
-
-#endif //LABIRYNT_DFS_H
--- solver.h
+++
@@ -1,9 +0,0 @@
-#ifndef LABIRYNT_SOLVER_H
-#define LABIRYNT_SOLVER_H
-
-#include "labirynt.h"
-
-int solver(komorka_t**, droga**, int x, int y, int* waga); //rozwiązuje labirynt
-int f(komorka_t** l, droga** d, int x, int y, int* waga, int nowa_waga); //funkcja pomocnicza do solver
-void writer(droga*, int); //wypisuje diagram przejść z wagami
-#endif //LABIRYNT_SOLVER_H
